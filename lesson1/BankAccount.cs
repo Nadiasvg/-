9533251a3e16720f147017bfb526f2a9203af2f7
Nadiasvg <nadia.16.kurnyavko@gmail.com>
2025-02-27 09:08:34 +0700

    Add files via upload

---
+++ lesson1/BankAccount.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using SystemOfBankAccount.ValueObjects;
+
+namespace SystemOfBankAccount
+{
+    class BankAccount
+    {
+        private static NumberOfBankAccount s_accountNumberSeed = new NumberOfBankAccount(1000000000);
+
+        private List<Transaction> _AllTransaction = new List<Transaction>();
+
+        public NumberOfBankAccount Number { get; }
+
+        public string Owner { get; private set; }
+
+        private decimal _balance = 0;
+
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in _AllTransaction)
+                {
+                    balance += item.Amount;
+                }
+                _balance = balance;
+                return balance;
+            }
+            private set
+            {
+
+            }
+        }
+
+        public BankAccount(string fullname, decimal initialBalance)
+        {
+            Number = s_accountNumberSeed;
+            s_accountNumberSeed.Value++;
+            Owner = fullname;
+            Balance = initialBalance;
+            Balance += 10m;
+
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(amount),
+                    "Amount of deposit must be positive");
+            var deposit = new Transaction(amount, date, note);
+            _AllTransaction.Add(deposit);
+
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (Balance - amount >= 0)
+                throw new InvalidOperationException("Not suffucuent rubbls for  this operation");
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(amount),
+                    "Amount of deposit must be positive");
+            var deposit = new Transaction(-amount, date, note);
+            _AllTransaction.Add(deposit);
+        }
+
+    }
+}
---
+++ lesson1/ValueObjects/NumberOfBankAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace SystemOfBankAccount.ValueObjects
+{
+    struct NumberOfBankAccount
+    {
+        public long Value { get; set; }
+        public NumberOfBankAccount(long number)
+        {
+            if (number < 1000000000 || number >= 10000000000)
+                throw new System.ArgumentOutOfRangeException(
+                    nameof(number),
+                    "Номер не является валидным");
+
+            Value = number;
+        }
+
+    }
+}
