5ae94bf964a815465f64385ff38ef537a2f0371b
Nadiasvg <nadia.16.kurnyavko@gmail.com>
2025-03-20 00:11:01 +0700

    Add files via upload

---
+++ домашняя 1/Tanks.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Tanks
+    {
+        /// <summary>
+        /// Unique tank identifier
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Volume { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int MaxVolume { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int UnitId {  get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public static Tanks[] GetTanks()
+        {
+            return new Tanks[] {
+            new Tanks { Id = 1, Name = "Резервуар 1", Description = "Надземно-вертикальный", Volume = 1500, MaxVolume = 2000,  UnitId = 1 },
+            new Tanks { Id = 2, Name = "Резервуар 2", Description = "Надземно-горизонтальный", Volume = 2500, MaxVolume = 3000,  UnitId = 1 },
+            new Tanks { Id = 3, Name = "Дополнительный резервуар 24", Description = "Надземно-горизонтальный", Volume = 3000, MaxVolume = 2000,  UnitId = 2},
+            new Tanks { Id = 4, Name = "Резервуар 35", Description = "Надземно-вертикальный", Volume = 3000, MaxVolume = 3000,  UnitId = 2 },
+            new Tanks { Id = 5, Name = "Резервуар 47", Description = "Подземно-двустенный", Volume = 4000, MaxVolume = 5000, UnitId = 2 },
+            new Tanks { Id = 6, Name = "Резервуар 256", Description = "Подводный", Volume = 500, MaxVolume = 500,  UnitId = 3 }
+            };
+        }
+        public static int GetTotalVolume(Tanks[] tanks)
+        {
+            int totalVolume = 0;
+            foreach (var tank in tanks)
+            {
+                totalVolume += tank.Volume;
+            }
+            return totalVolume;
+        }
+
+
+    }
+}
---
+++ домашняя 1/Units.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Units
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int FactoryId { get; set; }
+        /// <summary>
+        /// array of installations
+        /// </summary>
+        /// <returns></returns>
+        public static Units[] GetUnits()
+        {
+            return new Units[] {
+            new Units { Id = 1, Name = "ГФУ-2", Description = "Газофракционирующая установка", FactoryId = 1 },
+            new Units { Id = 2, Name = "АВТ-6", Description = "Атмосферно-вакуумная трубчатка", FactoryId = 1 },
+            new Units { Id = 3, Name = "АВТ-10", Description = "Атмосферно-вакуумная трубчатка", FactoryId = 2 } 
+            };
+        
+        }
+        /// <summary>
+        /// the factory object corresponding to the installation
+        /// </summary>
+        /// <param name="units"></param>
+        /// <param name="tanks"></param>
+        /// <param name="unitName"></param>
+        /// <returns></returns>
+        public static Units FindUnit(Units[] units, Tanks[] tanks, string tankName)
+        {
+            foreach (var tank in tanks)
+            {
+                if (tank.Name == tankName)
+                {
+                    foreach (var unit in units)
+                    {
+                        if (unit.Id == tank.UnitId)
+                        {
+                            return unit;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+
+    }
+}
