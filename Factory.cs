c8407fae06bf13a2c352444daee3934aabe398a3
Nadiasvg <nadia.16.kurnyavko@gmail.com>
2025-03-20 00:09:34 +0700

    Add files via upload

---
+++ Factory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Factory
+    /// </summary>
+    internal class Factory
+    {
+        public int Id {  get; set; }
+        public string Name {  get; set; }
+        public string Description {  get; set; }
+        public static Factory[] GetFactories()
+        {
+            return new Factory[] {
+            new Factory { Id = 1, Name = "НПЗ№1", Description = "Первый нефтеперерабатывающий завод" },
+            new Factory { Id = 2, Name = "НПЗ№2", Description = "Второй нефтеперерабатывающий завод" } 
+            };
+        }
+        public static Factory FindFactory(Factory[] factories, Units unit)
+        {
+            if (unit == null)
+            {
+                return null;
+            }
+
+            foreach (var factory in factories)
+            {
+                if (factory.Id == unit.FactoryId)
+                {
+                    return factory;
+                }
+            }
+            return null;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApp1;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var tanks = Tanks.GetTanks();
+        var units = Units.GetUnits();
+        var factories = Factory.GetFactories();
+        Console.WriteLine($"Количество резервуаров: {tanks.Length}, установок: {units.Length}");
+
+        var foundUnit = Units.FindUnit(units, tanks, "Резервуар 2");
+        var factory = Factory.FindFactory(factories, foundUnit);
+
+        Console.WriteLine($"Резервуар 2 принадлежит установке {foundUnit.Name} и заводу {factory.Name}");
+
+        var totalVolume = Tanks.GetTotalVolume(tanks);
+        Console.WriteLine($"Общий объем резервуаров: {totalVolume}");
+    }
+
+}
+
